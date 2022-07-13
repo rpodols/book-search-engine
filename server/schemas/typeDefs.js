0d9acc2a8a4a05c3c173a5b0d0639a9188df28e0
ryanp <rdpodols@gmail.com>
2022-07-12 21:59:22 -0400

    added typeDefs and apollo

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,28 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+    type Book {
+        _id: ID
+        authors: String
+        description: String
+        image: String
+        link: String
+        title: String
+
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+
+`;
+
+module.exports = typeDefs;
+
+
+  
