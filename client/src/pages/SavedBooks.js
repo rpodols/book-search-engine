14f300a099956aaae7973b375729b9114bd76853
ryanp <rdpodols@gmail.com>
2022-07-16 17:29:07 -0400

    added front end mutation logic for adding user and login, commented out original api code

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+//import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { useQuery, userMutation } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import { REMOVE_BOOK} from '../utils/mutations';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
