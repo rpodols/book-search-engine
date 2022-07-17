a59f1aafbb77e7c453a2bcf5c8bbd7451a4f675d
ryanp <rdpodols@gmail.com>
2022-07-17 14:32:38 -0400

    build

---
+++ client/build/precache-manifest.5d32fae93304b68fd531b7b504b5e25a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6496a7a9ce9e361725a90fef0c11d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2230c87d096328a6d7a",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4d91df3886aea976fce9",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "f2230c87d096328a6d7a",
+    "url": "/static/js/2.8109bc1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8109bc1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d91df3886aea976fce9",
+    "url": "/static/js/main.111e04b8.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
